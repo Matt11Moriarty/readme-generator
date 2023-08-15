d0d21067087499987064ef4a4da0f209a2075346
Matt11Moriarty <86008091+Matt11Moriarty@users.noreply.github.com>
2023-08-15 11:15:21 -0700

    added git ignore

--- index.js
+++ index.js
@@ -1,17 +1,23 @@
 // TODO: Include packages needed for this application
 
 const { generateMarkdown } = require("./utils/generateMarkdown");
+const fs = require('fs');
 
 // TODO: Create an array of questions for user input
 const questions = [
     {
         q1: "What is your title?"
+    },
+    {
+        q2: "Who is the author?"
     }
 ];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {
-
+    fs.writeFile('READ-ME.md', data, (err) => 
+        err ? console.log(err) : console.log('Readme generated')
+    )
 }
 
 // TODO: Create a function to initialize app
